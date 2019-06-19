2616f850fa48cd50365f38bb2558cc20c4c5423a
valeriafb <valeriafboch@gmail.com>
2019-06-18 23:22:06 -0300

    algunos atributos mas

--- 10. TP3/grafo.py
+++ 10. TP3/grafo.py
@@ -1,6 +1,7 @@
 class grafo:
     def __init__(self):
         self.vertices = {}
+        self.cant_vertices = 0;
 
     def adyacentes(self, vertice):
         res = []
@@ -8,8 +9,13 @@ class grafo:
             res.append(ady)
         return res
 
+    def agregar_vertice(self, v):
+        self.vertices[v] = set()
+        self.cant_vertices += 1
+
     def agregar_arista(self, v1, v2):
         if v1 not in self.vertices:
             self.vertices[v1] = {v2}
-        else:
+            self.cant_vertices += 1
+        else if v2 not in self.vertices[v1]:
             self.vertices[v1].add(v2)
