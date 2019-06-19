c9875815eafda705a0f629f595e7e51b8bb68dd1
valeriafb <valeriafboch@gmail.com>
2019-06-18 22:04:42 -0300

    makefile actualizado

--- 9. TP2/deps.mk
+++ 9. TP2/deps.mk
@@ -1,1 +1,5 @@
+CC := gcc
+CFLAGS := -g -std=c99 -Wall -Wtype-limits -pedantic -Wconversion -Wno-sign-conversion
+
 procesar_tweets: procesar_tweets.o strutil.o count_min_sketch.o hash.o lista.o tag.o heap.o pila.o
+procesar_usuarios: procesar_usuarios.o strutil.o hash.o lista.o heap.o
