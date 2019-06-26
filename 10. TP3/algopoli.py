a9e33ffd91047fd1f2b5f057fbe9968ed36f3e73
valeriafb <valeriafboch@gmail.com>
2019-06-26 20:58:36 -0300

    cargar un archivo en un grafo

---
+++ 10. TP3/algopoli.py
@@ -0,0 +1,11 @@
+import grafo
+import csv
+
+def cargar_archivo(ruta):
+    g = grafo.Grafo()
+    with open(ruta) as archivo:
+        arch = csv.reader(archivo, delimiter='\t')
+        for linea in arch:
+            v1, v2  = linea[0], linea[1]
+            g.agregar_arista(v1, v2)
+    return g
