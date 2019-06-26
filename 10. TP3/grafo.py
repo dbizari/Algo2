6191eb6fef7469edf35ff81602862b191309bdee
valeriafb <valeriafboch@gmail.com>
2019-06-26 19:20:10 -0300

    Creo que ya esta el tda, nose si hace falta el self. cant_vertices

--- 10. TP3/grafo.py
+++ 10. TP3/grafo.py
@@ -1,21 +1,44 @@
-class grafo:
+import random
+
+class Grafo:
     def __init__(self):
         self.vertices = {}
         self.cant_vertices = 0;
 
-    def adyacentes(self, vertice):
-        res = []
-        for ady in self.vertices[vertice]:
-            res.append(ady)
-        return res
+    def adyacentes(self, v):
+        adyacentes = []
+        for ady in self.vertices[v]:
+            adyacentes.append(ady)
+        return adyacentes
 
     def agregar_vertice(self, v):
-        self.vertices[v] = set()
-        self.cant_vertices += 1
+        if not v in self.vertices:
+            self.vertices[v] = set()
+            self.cant_vertices += 1
+
+    def borrar_vertice(self, v):
+        del self.vertices[v]
+        self.cant_vertices-=1
 
     def agregar_arista(self, v1, v2):
+        if v1 == v2: return
         if v1 not in self.vertices:
-            self.vertices[v1] = {v2}
-            self.cant_vertices += 1
-        else if v2 not in self.vertices[v1]:
-            self.vertices[v1].add(v2)
+            self.agregar_vertice(v1)
+        self.vertices[v1].add(v2)
+
+    def borrar_arista(self, v1, v2):
+        aux = self.vertices[v1]
+        aux.remove(v2)
+
+    def obtener_vertices(self):
+        vertices = []
+        for v in self.vertices:
+            vertices.append(v)
+        return vertices
+
+    def obtener_vertice_random(self):
+        return random.choice(list(self.vertices.keys()))
+
+    def estan_unidos(self, v1, v2):
+        if v2 in self.vertices[v1]: return True
+        return False
