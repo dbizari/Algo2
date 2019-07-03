a7cb19ed8314ab4e87bab1f0b030ee6f54c7fd96
dbizari <d.bizari@gmail.com>
2019-07-02 23:59:57 -0300

    Corregí algunos, bugs, agregué método adyacentes(), e hice múltiples pruebas unitarias

--- 10. TP3/grafo.py
+++ 10. TP3/grafo.py
@@ -12,11 +12,12 @@ class Grafo:
         return adyacentes
 
     def agregar_vertice(self, v):
-        if not v in self.vertices:
+        if v not in self.vertices:
             self.vertices[v] = set()
             self.cant_vertices += 1
 
     def borrar_vertice(self, v):
+        if v not in self.vertices: return
         del self.vertices[v]
         self.cant_vertices-=1
 
@@ -29,14 +30,19 @@ class Grafo:
         self.vertices[v1].add(v2)
 
     def borrar_arista(self, v1, v2):
-        aux = self.vertices[v1]
-        aux.remove(v2)
+        if v1 not in self.vertices or v2 not in self.vertices:
+            return
+        self.vertices[v1].discard(v2) #Cambie remove por discard por si no se encuentra presente
 
     def obtener_vertices(self):
         vertices = []
         for v in self.vertices:
             vertices.append(v)
         return vertices
+
+    def adyacentes(self,v):
+        if v not in self.vertices: return None
+        return list(self.vertices[v])
 
     def obtener_vertice_random(self):
         return random.choice(list(self.vertices.keys()))
@@ -47,3 +53,19 @@ class Grafo:
 
     def __iter__(self):
         return iter(self.vertices)
+
+graph = Grafo()
+graph.agregar_vertice(9)
+graph.agregar_vertice(4)
+graph.agregar_vertice(5)
+graph.agregar_vertice(6)
+graph.agregar_vertice(7)
+graph.borrar_vertice(5)
+graph.borrar_vertice(6)
+graph.agregar_arista(7,6)
+graph.agregar_arista(5,6)
+graph.agregar_arista(9,4)
+graph.agregar_arista(9,5)
+graph.agregar_arista(9,7)
+
+print(graph.obtener_vertice_random())
