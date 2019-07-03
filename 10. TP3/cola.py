f3567377bd808930a41862335e8f7df4785665b8
dbizari <d.bizari@gmail.com>
2019-07-02 22:10:24 -0300

    Agregué el atributo de largo a la clase con su respectivo getter y cambié el método de desencolar()

--- 10. TP3/cola.py
+++ 10. TP3/cola.py
@@ -2,6 +2,7 @@ class Cola:
     def __init__(self):
         self.prim = None
         self.ult = None
+        self.largo = 0
 
     def encolar(self, dato):
         nodo = _Nodo(dato)
@@ -10,22 +11,37 @@ class Cola:
         else:
             self.ult.prox = nodo
         self.ult = nodo
+        self.largo += 1
 
     def desencolar(self):
         nodo = self.prim
+        if nodo == None:
+            return None
         if nodo == self.ult:
             self.ult = None
-        if nodo:
-            self.prim = nodo.prox
-            return nodo.dato
-        else:
-            return None
+        self.prim = nodo.prox
+        self.largo -= 1
+        return nodo.dato
 
     def esta_vacia(self):
         return self.prim == None
 
+    def ver_largo(self):
+        return self.largo
 
 class _Nodo:
     def __init__(self, dato):
         self.dato  = dato
         self.prox = None
+
+q = Cola()
+print(q.desencolar())
+q.encolar(5)
+q.encolar(6)
+q.encolar(7)
+q.encolar(8)
+print(q.desencolar())
+print(q.desencolar())
+print(q.desencolar())
+print(q.desencolar())
+print(q.desencolar())
