e050f83159536b1b31a4f552f764826f2b304633
valeriafb <valeriafboch@gmail.com>
2019-07-04 17:51:45 -0300

    hice min_seguimientos aunque tarda demasiado

--- 10. TP3/algopoli.py
+++ 10. TP3/algopoli.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+from pila import Pila
 from grafo import Grafo
 import csv
+import biblioteca
 
 COMANDOS = ["min_seguimientos", "mas_imp", "persecucion", "comunidades", "divulgar", "divulgar_ciclo", "cfc"]
 
@@ -16,3 +18,23 @@ def cargar_archivo(ruta):
 
 def main(ruta):
     grafo = cargar_archivo(ruta)
+    comando = input()
+    comando = comando.split(' ')
+    if comando[0] == COMANDOS[0]:
+        distancia, padre = biblioteca.camino_minimo_bfs(grafo, comando[1])
+        if comando[2] not in distancia:
+            print("Seguimiento imposible\n")
+        else:
+            p = Pila()
+            aux = comando[2]
+            while aux!=comando[1]:
+                p.apilar(aux)
+                aux = padre[aux]
+            p.apilar(comando[1])
+            while not p.esta_vacia():
+                if p.ver_tope()!=comando[2]:
+                    print(f"{p.desapilar()} -> ", end = '')
+                else:
+                    print(f"{p.desapilar()}")
+
+main("mensajes.tsv")
