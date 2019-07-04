0350b4aec7a01558e9aad03342a19ae834801b0d
valeriafb <valeriafboch@gmail.com>
2019-07-04 17:55:51 -0300

    min_seg en funcion aparte

--- 10. TP3/algopoli.py
+++ 10. TP3/algopoli.py
@@ -16,25 +16,28 @@ def cargar_archivo(ruta):
             g.agregar_arista(v1, v2)
     return g
 
+def min_seguimientos(grafo, p1, p2):
+    distancia, padre = biblioteca.camino_minimo_bfs(grafo, p1)
+    if p2 not in distancia:
+        print("Seguimiento imposible\n")
+    else:
+        p = Pila()
+        aux = p2
+        while aux!=p1:
+            p.apilar(aux)
+            aux = padre[aux]
+        p.apilar(p1)
+        while not p.esta_vacia():
+            if p.ver_tope()!=p2:
+                print(f"{p.desapilar()} -> ", end = '')
+            else:
+                print(f"{p.desapilar()}")
+
 def main(ruta):
     grafo = cargar_archivo(ruta)
     comando = input()
     comando = comando.split(' ')
     if comando[0] == COMANDOS[0]:
-        distancia, padre = biblioteca.camino_minimo_bfs(grafo, comando[1])
-        if comando[2] not in distancia:
-            print("Seguimiento imposible\n")
-        else:
-            p = Pila()
-            aux = comando[2]
-            while aux!=comando[1]:
-                p.apilar(aux)
-                aux = padre[aux]
-            p.apilar(comando[1])
-            while not p.esta_vacia():
-                if p.ver_tope()!=comando[2]:
-                    print(f"{p.desapilar()} -> ", end = '')
-                else:
-                    print(f"{p.desapilar()}")
+        min_seguimientos(grafo, comando[1], comando[2])
 
 main("mensajes.tsv")
